b5d193883f6d3bbfb1caed27b134d7b3513d9e55
zj <zoujian@83chedai.com>
2017-02-20 18:04:01 +0800

    解决csrf无法正确设置的问题

--- crawlmanager/apps/foundation/views.py
+++ crawlmanager/apps/foundation/views.py
@@ -1,14 +1,12 @@
 from django.http import HttpResponse
-from django.template import Context
-from django.template.loader import get_template
+from django.shortcuts import render
 
 from rest_framework.views import View
 
 
 class Home(View):
     def get(self, request):
-        t = get_template("foundation/index.html")
-        return HttpResponse(t.render(Context({'title': 'test'})))
+        return render(request, 'foundation/index.html', {'title': 'haha'})
 
     def post(self, request):
         return HttpResponse('hello world!')
