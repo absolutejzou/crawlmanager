0df54fc84df915ecd7eaea2ed5f3e1dd93403d7e
zj <zoujian@83chedai.com>
2017-05-18 22:15:25 +0800

    首页

--- websites/static/common/javascript/common.js
+++ websites/static/common/javascript/common.js
@@ -1,8 +1,23 @@
-$.ajaxSetup({
-    complete: function (xhr, status, e) {
-        if ( xhr.status == 278 ) {
-            window.location.href = xhr.getResponseHeader("Location");
-        }
-    },
-    traditional: true
+$(function() {
+    $.ajaxSetup({
+        complete: function (xhr, status, e) {
+            if ( xhr.status == 278 ) {
+                window.location.href = xhr.getResponseHeader("Location");
+            }
+        },
+        traditional: true
+    });
+
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrainWidth: true,
+        hover: false,
+        gutter: 0,
+        belowOrigin: true,
+        alignment: 'left',
+        stopPropagation: true
+    });
+
+    $('#side-button').sideNav();
 });
